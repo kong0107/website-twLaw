be0241a91ee9a9304b1228a3a9fd399aa8ec4a0f
Kong Kao <kong0107@gmail.com>
2016-01-22 16:13:30 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,26 @@
+#!/usr/bin/env bash
+#
+# Only for Openshift to download law database from GitHub
+#
+# One argument is required: the tag name for the GitHub repository.
+# The JSON files are reserved in case `import.js` is updated and you wanna use them again.
+#
+[ "$#" -eq 1 ] || exit 1
+
 cd $OPENSHIFT_DATA_DIR
 wget  https://github.com/kong0107/mojLawSplitJSON/archive/$1.tar.gz
+if [[ $? -ne 0 ]] ; then
+	exit 1
+fi
+
 rm -rf json
 tar -zx -f $1.tar.gz
+rm $1.tar.gz
 mv mojLawSplitJSON-$1 json
+
 rm -rf json/HisMingLing
 rm -rf json/Eng_MingLing
 rm -rf json/Eng_FalV
 mv json/UpdateDate.txt ${OPENSHIFT_REPO_DIR}data
+
 node ${OPENSHIFT_REPO_DIR}import.js

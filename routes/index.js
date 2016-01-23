7dd4fc6b3e52a0ed4c0ffe953dea76b8aaf43b31
Kong Kao <kong0107@gmail.com>
2016-01-23 23:01:55 +0800

    show latest updates at homepage

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,21 @@
 var config = require('../config.js');
 var express = require('express');
+var parser = require('../libs/parser.js');
 
 var router = module.exports = express.Router();
 var model = { config: config };
 
 router.get('/', function(req, res) {
-	res.render('index', model);
+	config.db.collection('latest').find().sort({'最新異動日期': -1}).limit(20)
+	.project({'法規內容': false}).toArray(function(err, docs) {
+		for(var i = 0; i < docs.length; ++i) {
+			docs[i].lastUpdateMessage = docs[i].沿革內容.match(/[^.]*$/)[0]
+				.replace(/\s+/g, '').replace(/([\w\-、～]+)/g, " $1 ")
+			;
+		}
+		model.latest = docs;
+		res.render('index', model);
+	});
 });
 
 router.get('/law/:name', function(req, res, next) {

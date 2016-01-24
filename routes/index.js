82f14b8317f4808b3d32146b699d45777e7e1f6c
Kong Kao <kong0107@gmail.com>
2016-01-25 01:02:12 +0800

    make index mimic law-index

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,12 @@ var router = module.exports = express.Router();
 var model = { config: config };
 
 router.get('/', function(req, res) {
-	res.redirect('/law');
+	config.db.collection('latest').find().sort({'最新異動日期': -1}).limit(20)
+	.project({'法規內容': false}).toArray(function(err, docs) {
+		docs.forEach(function(doc) {
+			doc.沿革內容 = parser.parseHistory(doc.沿革內容);
+		});
+		model.lawList = docs;
+		res.render('law-index', model);
+	});
 });

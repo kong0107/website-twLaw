1930d7d8d6b5df63f9386bbe1e6cebf90811077b
Kong Kao <kong0107@gmail.com>
2016-01-26 01:36:24 +0800

    delete unused codes

--- libs/parser.js
+++ libs/parser.js
@@ -12,6 +12,8 @@ parser.parseHistory = function(str) {
  * 將條文內容字串，轉為巢狀結構
  *
  * 憲法第 49 條其實沒有分項，但從全國法規資料庫是看不出來的，須經由立法院法律系統確認。
+ * 國際法中譯版會有問題，例如「維也納條約法公約」第7條第1項（甲）款末尾是「；或」。
+ *
  * @param {string} str 以換行做為排版的條文內容字串。
  * @return {object} 物件有成員：
  * * text {string}: 主文。
@@ -32,7 +34,6 @@ parser.parseArticle = function(str) {
 		var trimmed = lines[i].trim();
 		if(isNew && trimmed.charAt(0) != '但') eles.push({text: lines[i]}); ///< 例如民法第95條
 		else eles[eles.length - 1].text += trimmed;
-		//if(!isNew) eles[eles.length - 1].spacesDebug = lines[i].match(/\s*/)[0].length;
 		isNew = /(：|︰|。|（刪除）)$/.exec(trimmed);
 	}
 	
@@ -41,7 +42,6 @@ parser.parseArticle = function(str) {
 		for(var j = stratums.length - 1; j >= 0; --j) {
 			var match = stratums[j].ordinal.exec(eles[i].text);
 			if(match) break;
-			//if(stratums[j].ordinal.test(eles[i].text)) break;
 		}
 		eles[i].stratum = j;
 		
@@ -110,71 +110,6 @@ var stratums = [
         "ordinal": /^\s+（\d+）/   ///< 全形括號（與立法院不同）、半形數字
     }
 ];
-/**
- * 老方法，輸出 HTML 。
- * 
- * @see https://github.com/g0v/laweasyread-front/blob/master/pre32.js
- */
-parser.parseArticleToHTML = function(str) {
-	var html = "<li>";
-	var depthArr = [];
-	var lines = str.split(/\r?\n/);
-	if(lines[lines.length - 1]) lines.push('');
-    for(var j = 0; j < lines.length; ++j) {
-        var endDetect = /(：|︰|。|（刪除）)$/.exec(html); ///< 兩種冒號不同
-        if(endDetect) {
-            var stratum = -1;
-            for(var k = stratums.length - 1; k >= 0; --k) {
-                if(stratums[k].ordinal.test(lines[j])) {
-                    stratum = k;
-                    break;
-                }
-            }
-            
-            /// 唔，有些分款的項是用句點結尾的，先這樣繞過去吧...
-            if(stratum >= 0 && lines[j].indexOf("一、") == 0) endDetect[1] = "：";
-            
-            switch(endDetect[1]) {
-            case "。":
-                /// 處理所得稅法中，同一層之中又分段的情形，例如14條1項2類
-                if(stratum < 0) {
-                    html += "<br />";
-                    break;
-                }
-                /// 警告可能的錯誤情形
-                if(!depthArr.length             ///< 「項」
-                    && lines[j-1].length == 32  ///< 前一行有32字
-                    && !/\w/.test(lines[j-1])   ///< 且沒有英數
-                    && lines[j]                 ///< 而這行不是最後一行
-                ) html += '<div class="LER-warning" title="原始資料用「換行」的方式排版，因而本站沒能判斷這裡究竟是「換行」還是「分項」。">警告：這裡可能其實沒有分項。</div>';
-                // no break!
-            case "（刪除）":
-                html += "</li>";
-                while(depthArr.length && depthArr[depthArr.length - 1] != stratum) {
-                    html += "</ol></li>";
-                    depthArr.pop();
-                }   
-                break;
-            case "：":
-            case "︰":   ///< 這兩個冒號不同
-                /// 處理憲法§48
-                /// 其實有「算式」的條文也會出錯，例如所得稅法§66-6的第一項和第二項就很難分
-                if(lines[j][0] == "「") {
-                    stratum = -1;
-                    break; 
-                }
-                html += '<ol class="LER-art-' + stratums[stratum].name + '">';
-                depthArr.push(stratum);
-                break;
-            default: 
-                console.log(endDetect);
-                throw new SyntaxError("RegExp returns wrong match.");
-            }
-            if(lines[j] && stratum >= 0) html += "<li>"; ///< 如果是末行的空白就不用加了
-        }            
-        html += lines[j].trim();	///< 有些條文在冒號後還會有空白，例如民訴§§307,541
-    }
-	return html;
-};
+
 
 module.exports = parser;
